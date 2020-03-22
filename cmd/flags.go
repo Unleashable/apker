e1f9ef5d22398ad425ab2e8e85057ecc391d3961
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-03-22 14:22:58 +0100

    init cmd/flags.go

---
+++ cmd/flags.go
@@ -0,0 +1,21 @@
+// Copyright 2020 Mohammed El Bahja. All rights reserved.
+// Use of this source code is governed by a MIT license.
+
+package cmd
+
+import (
+	"github.com/urfave/cli/v2"
+)
+
+var flags = []cli.Flag{
+	&cli.StringFlag{
+		Name:    "pub",
+		Aliases: []string{"p"},
+		Usage:   "Set ssh public key (`public_key`)",
+	},
+	&cli.StringFlag{
+		Name:    "key",
+		Aliases: []string{"i"},
+		Usage:   "Set ssh private key (`identity_file`)",
+	},
+}
