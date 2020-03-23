b28bc897ea023aa76a2f920bc4e62aba92219343
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-03-23 23:43:24 +0100

    add: string input

---
+++ cmd/inputs/string.go
@@ -0,0 +1,23 @@
+// Copyright 2020 Mohammed El Bahja. All rights reserved.
+// Use of this source code is governed by a MIT license.
+
+package inputs
+
+import (
+	"github.com/manifoldco/promptui"
+)
+
+func AskString(label string, def string) (v string, e error) {
+
+	prompt := promptui.Prompt{
+		Label: label,
+	}
+
+	v, e = prompt.Run()
+
+	if e == nil && v == "" {
+		v = def
+	}
+
+	return
+}
