e685491e12a09d93e680253e151ee73d44b36184
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-03-23 23:45:36 +0100

    new struct: internal/provider.go added.

---
+++ internal/provider.go
@@ -0,0 +1,30 @@
+// Copyright 2020 Mohammed El Bahja. All rights reserved.
+// Use of this source code is governed by a MIT license.
+
+package internal
+
+type Attributes map[string]interface{}
+
+type MachineStatus struct {
+	IsImageReady     bool
+	IsImageInstalled bool
+	IsMachineReady   bool
+	Status           string
+	Error            error
+}
+
+type Machine struct {
+	ID     int
+	Addr   string
+	Name   string
+	Region string
+	Status string
+}
+
+type Provider interface {
+
+	// Setup virtual machine on cloud provider
+	SetupMachine(chan MachineStatus, Attributes)
+
+	// List() []Machine
+}
