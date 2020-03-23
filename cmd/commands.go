d696aebdd26d0ca773dcba6facba1b99d28dec38
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-03-23 23:50:36 +0100

    move flags to actions

--- cmd/commands.go
+++ cmd/commands.go
@@ -8,50 +8,13 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-var commands = []*cli.Command{
+var Commands = []*cli.Command{
 	{
 		Name:    "deploy",
 		Aliases: []string{"dep"},
-		Usage:   "Deploy from current working directory or remote git repo.",
+		Usage:   "Deploy from remote git repository.",
 		Action:  actions.Deploy,
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:  "name",
-				Usage: "Set image name.",
-			},
-			&cli.StringFlag{
-				Name:  "size",
-				Usage: "Set image size.",
-			},
-			&cli.StringFlag{
-				Name:    "region",
-				Aliases: []string{"location"},
-				Usage:   "Set image physical location.",
-			},
-			&cli.StringFlag{
-				Name:    "pass",
-				Aliases: []string{"passphrase"},
-				Usage:   "SSH passphase.",
-			},
-			&cli.BoolFlag{
-				Name:    "ssh",
-				Aliases: []string{"enable-ssh"},
-				Usage:   "Use ssh key instead of password.",
-			},
-			&cli.BoolFlag{
-				Name:    "wait",
-				Aliases: []string{"w"},
-				Usage:   "Wait for image to be ready (without timeout).",
-			},
-			&cli.IntFlag{
-				Name:  "id",
-				Usage: "Run deploy steps on specifec droplet `id`.",
-			},
-			&cli.IntFlag{
-				Name:  "image",
-				Usage: "Create droplet from specifec image `id`.",
-			},
-		},
+		Flags:   actions.DeployFlags,
 	},
 	{
 		Name:    "list",
