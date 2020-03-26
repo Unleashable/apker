bf9d48946e7734f506d9212b180e0b7b5ad67b67
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-03-26 19:40:35 +0100

    add: nice ui outputs!

---
+++ cmd/outputs/ui.go
@@ -0,0 +1,33 @@
+package outputs
+
+import (
+	"fmt"
+	"github.com/manifoldco/promptui"
+)
+
+func Success(msg string, icon string) {
+
+	if icon == "" {
+		icon = "✔"
+	}
+
+	fmt.Println(Render(icon, msg, true))
+}
+
+func Error(msg string, icon string) {
+
+	if icon == "" {
+		icon = "✗"
+	}
+
+	fmt.Println(Render(icon, msg, false))
+}
+
+func Render(icon string, msg string, isSuccess bool) string {
+
+	if isSuccess {
+		return promptui.Styler(promptui.FGGreen)(icon) + " " + promptui.Styler(promptui.FGFaint)(msg)
+	}
+
+	return promptui.Styler(promptui.FGRed)(icon) + " " + promptui.Styler(promptui.FGFaint, promptui.FGBold)(msg)
+}
