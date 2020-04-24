f6cfb37e632ea17808c12cc3b982eeaee8ca6de7
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-04-24 15:01:16 +0000

    new way to run actions.

--- cmd/actions/run.go
+++ cmd/actions/run.go
@@ -9,9 +9,7 @@ import (
 	"strings"
 
 	"github.com/melbahja/goph"
-	. "github.com/unleashable/apker/cmd/utils"
-	"github.com/unleashable/apker/internal"
-	"github.com/unleashable/apker/internal/utils"
+	"github.com/unleashable/apker/cmd/inputs"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/crypto/ssh"
 )
@@ -28,21 +26,32 @@ var RunFlags = []cli.Flag{
 		Value: "root",
 		Usage: "Set ssh `user` name.",
 	},
+	&cli.StringFlag{
+		Name:    "key",
+		Value:   os.ExpandEnv("$HOME/.ssh/id_rsa"),
+		Usage:   "Set private key path.",
+		Aliases: []string{"i"},
+	},
+	&cli.StringFlag{
+		Name:  "knownhosts",
+		Usage: "knownhosts `file`.",
+		Value: os.ExpandEnv("$HOME/.ssh/known_hosts"),
+	},
 	&cli.BoolFlag{
 		Name:  "passphrase",
 		Usage: "Ask for private key passphrase for protected keys.",
 	},
 	&cli.BoolFlag{
+		Name:  "agent",
+		Usage: "Use ssh agent.",
+	},
+	&cli.BoolFlag{
 		Name:  "password",
 		Usage: "Ask for ssh password instead of using private keys.",
 	},
-	&cli.BoolFlag{
-		Name:  "insecure",
-		Usage: "Do not check knownhosts.",
-	},
 	&cli.StringSliceFlag{
 		Name:    "env",
-		Usage:   "Set action env variables.",
+		Usage:   "Set action env variables: (`NAME=VALUE`).",
 		Aliases: []string{"e"},
 	},
 }
@@ -51,60 +60,59 @@ func Run(c *cli.Context) (e error) {
 
 	var (
 		cmd      string = fmt.Sprintf("/usr/share/apker/bin/%s", c.Args().First())
+		pass     string
+		auth     goph.Auth
 		client   *goph.Client
 		output   []byte
 		callback ssh.HostKeyCallback
-		project  internal.Project = internal.Project{
-			Temp: utils.Temp(),
-		}
 	)
 
-	//Housekeeping.
-	defer os.RemoveAll(project.Temp)
-
-	// Set auth method.
-	if e = SetAuthMethod(&project, c); e != nil {
+	if callback, e = goph.KnownHosts(c.String("knownhosts")); e != nil {
 		return
 	}
 
-	// Set host key callback.
-	if c.Bool("insecure") {
+	if c.Bool("password") {
 
-		callback = ssh.InsecureIgnoreHostKey()
+		pass, e = inputs.Password("Enter ssh password", func(pass string) error {
+			return nil
+		})
 
-	} else if callback, e = goph.DefaultKnownHosts(); e != nil {
+		if e != nil {
+			return
+		}
 
-		return
+		auth = goph.Password(pass)
+
+	} else if c.Bool("agent") {
+
+		auth = goph.UseAgent()
+
+	} else {
+
+		if c.Bool("passphrase") {
+
+			pass, e = inputs.Password("Enter private key passphrase", func(pass string) error {
+				return nil
+			})
+		}
+
+		auth = goph.Key(c.String("key"), pass)
 	}
 
 	// Get ssh client.
-	if client, e = goph.NewConn(c.String("user"), c.String("addr"), project.SSHAuth, callback); e != nil {
+	if client, e = goph.NewConn(c.String("user"), c.String("addr"), auth, callback); e != nil {
 		return
-	}
-
-	// Get apker config file from remote machine.
-	if e = client.Download("/usr/share/apker/apker.yaml", project.Temp+"/apker.yaml"); e != nil {
-		return
-	}
-
-	// Load project config from apker.yaml
-	if project.Config, e = internal.LoadConfig(project.Temp, []string{}); e != nil {
-		return
-	}
-
-	// Override if action on apker file.
-	if action := project.Config.GetAction(c.Args().First()); action != "" {
-		cmd = action
 	}
 
 	// Run the action.
-	if output, e = client.Run(fmt.Sprintf(`env %s bash -c '%s'`, env(c.StringSlice("env")), cmd)); e == nil {
-		fmt.Println(string(output))
-	}
+	output, e = client.Run(fmt.Sprintf(`env %s bash -c '%s'`, env(c.StringSlice("env")), cmd))
+
+	fmt.Println("")
+	fmt.Println(string(output))
 
 	return e
 }
 
 func env(s []string) string {
-	return strings.Join(append(s, "APKER_ACTION=yes"), " ")
+	return strings.Join(append(s, "APKER_ACTION=1"), " ")
 }
